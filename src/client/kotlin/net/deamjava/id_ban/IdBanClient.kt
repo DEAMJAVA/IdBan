3aca95209f7edfd86c37db8a13b86d5e238ca1ba
DEAMJAVA <deamminecraft3@gmail.com>
2026-04-13 17:38:31 +0530

    Updated Fabric Loader and fixed Minecraft Client crashing if mod installed on client

--- src/client/kotlin/net/deamjava/id_ban/IdBanClient.kt
+++
@@ -1,9 +0,0 @@
-package net.deamjava.id_ban
-
-import net.fabricmc.api.ClientModInitializer
-
-object IdBanClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
